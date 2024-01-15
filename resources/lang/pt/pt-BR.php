947f2f9692ea857b76c7efe55285d153f9fd9004
William Pinheiro <william.pinheiro@eletromidia.com.br>
2024-01-15 20:01:15 -0300

    + remove lang pt-br

---
+++ resources/lang/pt/pt-BR.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'Login' => 'Login',
+    'Email Address' => 'Endereço de E-mail',
+    'Password' => 'Senha',
+    'Confirm Password' => 'Confirmar Senha',
+    'Send Password Reset Link' => 'Recuperar Senha',
+    'Remember Me' => 'Lembrar de mim',
+    'Enter' => 'Entrar',
+    'Forgot Your Password?' => 'Esqueceu sua senha?',
+    'Please confirm your password before continuing.' => 'Por favor confirme sua senha para continuar.',
+    'month' => 'Mês',
+    'day' => 'Dias',
+    'year' => 'Anos',
+    'date_formats' => [
+        'year' => 'M',
+        'day' => 'd/m',
+        'month' => 'd/m'
+    ],
+    
+];
